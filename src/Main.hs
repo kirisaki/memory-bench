1956b93bd9e274500834d73e4a729341405a381e
Akihito KIRISAKI <kirisaki@klaraworks.net>
2018-12-24 23:27:57 +0900

    Add benchmark

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Concurrent
 import Control.Monad
 import Criterion.Main
+import Foreign.ForeignPtr
 import Foreign.Ptr
 import Foreign.Marshal.Alloc
 import Foreign.Storable
@@ -13,8 +14,8 @@ import System.Random
 type Socket = ()
 
 bufLen = 4096
-bufNum = 100
-threads = 1
+bufNum = 10
+threads = 2
 
 recvBuf :: Socket -> Ptr Word8 -> Int -> IO Int
 recvBuf _ ptr len = do
@@ -22,11 +23,21 @@ recvBuf _ ptr len = do
     val <- randomIO :: IO Word8
     ptr `plusPtr` off `poke` val
   return len
-  
-withNewPinned :: IO ()
-withNewPinned = replicateM_ bufNum (void $ allocaBytes bufLen (\ptr -> void $ recvBuf () ptr bufLen))
+
+withBuf :: ((Ptr Word8 -> IO ()) -> IO ()) -> IO ()
+withBuf f = replicateM_ bufNum (forkIO . void $ f (\ptr -> void $ recvBuf () ptr bufLen))
+
+withAlloca :: IO ()
+withAlloca = withBuf $ allocaBytes bufLen
+
+withMalloc :: IO ()
+withMalloc = do
+  ptr <- mallocBytes bufLen
+  ptr' <- newForeignPtr finalizerFree ptr
+  withBuf $ withForeignPtr ptr'
 
 main :: IO ()
 main = defaultMain
-  [ bench "test" $ whnfIO $ withNewPinned
+  [ bench "alloca" $ whnfIO $ withAlloca
+  , bench "malloc" $ whnfIO $ withMalloc
   ]
